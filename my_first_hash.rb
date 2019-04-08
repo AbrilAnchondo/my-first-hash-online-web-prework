ecb809a4e2655b8c132171067e205febb13c75b3
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-08 04:37:33 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   
-   your_hash = {"name" => "Abril", "last_name" => "Anchondo"}
+   your_hash = {"name" => "Abril", "last name" => "Anchondo"}
   end
 
 
